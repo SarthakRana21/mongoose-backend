94c37acc5ded687ad5db082343addfe5c8bc9f85
= <sarthak.rana0321@gmail.com>
2025-02-21 16:24:00 +0530

    added ApiError ApiResponse asyncHandler uitls

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,26 @@
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err) => {
+            next(err)
+        })
+    }
+}
+export {asyncHandler}
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = async (fn) => () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//  try {
+//     await fn(req, res, next)
+//  } catch (error) {
+//     res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message
+//     })
+//  }
+// }
