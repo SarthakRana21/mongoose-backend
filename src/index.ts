9c8d5d9be136418c83fcd67bb105321143685dd8
= <sarthak.rana0321@gmail.com>
2025-02-21 16:23:38 +0530

    DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { config } from "dotenv";
-import connectDB from "./db/index";
-
+import connectDB from "./db/db";
+import {app} from "./app";
 config({path: './.env'})
 
+const port = process.env.PORT || 8000
 
 connectDB()
+.then(() => {
+    
+    app.on('error', (err) => {
+        console.log('Error on App: ', err)  
+        throw err  
+    })
+    
+    app.listen(port, () => {
+        console.log(`server is running on port: ${port}`)
+    })
+    
+}) 
+.catch((err) => {
+    console.log("MongoDB Connection Failed xxx")
+})
 
 
 
