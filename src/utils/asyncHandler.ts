78e9e83c6805e82e102ed7d74fc83f1b52d37c56
= <sarthak.rana0321@gmail.com>
2025-02-25 17:51:05 +0530

    fixed return error

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,5 +1,7 @@
-const asyncHandler = (reqHandler) => {
-    (req, res, next) => {
+import { NextFunction, Request, Response } from "express"
+
+const asyncHandler = (reqHandler: (req: Request, res: Response, next: NextFunction) => Promise<any>)  => {
+    return (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(reqHandler(req, res, next))
         .catch((err) => {
             next(err)
