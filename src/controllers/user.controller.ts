552dd52d2f56610f942552e9e9636ce9fc377954
= <sarthak.rana0321@gmail.com>
2025-02-25 19:17:30 +0530

    added regester logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,28 @@
 import {asyncHandler} from '../utils/asyncHandler';
+import { ApiError } from '../utils/ApiError';
+import {User} from '../models/user.model'
 
 const registerUser = asyncHandler(async (req, res)  => {
+
+    const {username, email, fullName, avatar, coverImage, password} = req.body 
+    const userFields = [username, email, fullName, avatar, coverImage, password]
+    
+    if (userFields.some((item) => item?.trim() === "")) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const extestedUser = await User.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    if (extestedUser) {
+        throw new ApiError(400, "Username and email must be unique")
+    }
+
+
     res.status(200).json({
         message: "ok",
         data: "meow"
