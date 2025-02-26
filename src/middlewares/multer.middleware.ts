a3b2811748191a0651569666f79f6d15844dd6ff
= <sarthak.rana0321@gmail.com>
2025-02-26 16:34:24 +0530

    fixed destination path

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -2,10 +2,10 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './pulic/temp')
+        cb(null, './public/temp')
     },
     filename: function(req, file, cb) {
-        cb(null, file.filename)
+        cb(null, `${Date.now()}-${file.filename}`)
     }
 })
 
