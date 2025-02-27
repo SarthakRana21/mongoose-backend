2facf15228fd97f0a336d2c9940c2f6fc4d163a4
= <sarthak.rana0321@gmail.com>
2025-02-27 16:07:48 +0530

    complete register logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,31 +3,12 @@ import { ApiError } from '../utils/ApiError';
 import {User} from '../models/user.model'
 import {uploadOnCloudidnary} from '../utils/cloudinary'
 import {ApiResponse} from '../utils/ApiResponse'
+import fs from 'fs'
 
 const registerUser = asyncHandler(async (req, res)  => {
 
     const {username, email, fullName, avatar, coverImage, password} = req.body 
-    const userFields = [username, email, fullName, avatar, coverImage, password]
-    
-    // if (userFields.some((item) => item?.trim() === "")) {
-    //     throw new ApiError(400, "All fields are required")
-    // }
-
-    for (const item of userFields) {
-        if (item?.trim() === "") {
-            throw new ApiError(400, `${item} is a required field`);
-        }
-    }
-    
-    // const extestedUser = await User.findOne({
-    //     $or: [ {username}, {email} ]
-    // })
-
-    const emailExist = await User.findOne({email: email})
-    const usernameExist = await User.findOne({username: username})
-
-    if (emailExist) throw new ApiError(400, "email must be unique")
-    if (usernameExist) throw new ApiError(400, "Username must be unique")
+    const userFields = [username, email, fullName, password]
 
     const files = req.files as {
         avatar?: Express.Multer.File[],
@@ -36,36 +17,58 @@ const registerUser = asyncHandler(async (req, res)  => {
 
     const avatarLocalPath = files.avatar?.[0]?.path || null;
     const coverImageLocalPath = files.coverImage?.[0]?.path || null;
+    
+    try {
+        if (userFields.some((item) => !item || item?.trim() === "")) {
+            throw new ApiError(400, "All fields are required")
+        }
+        
+        const userExist = await User.findOne({
+            $or: [
+                {email}, {username}
+            ]
+        })
+    
+        if(userExist) {
+            
+            throw new ApiError(400, userExist.email == email ? "Email must be unique": "Username must be unique");
+        }
+    
+        if (!avatarLocalPath) {
+            throw new ApiError(400, "Please upload avatar pic")
+        }
+    
+        const avatarIMG = await uploadOnCloudidnary(avatarLocalPath)
+        const coverImageIMG = await uploadOnCloudidnary(coverImageLocalPath)
+    
+        if (!avatarIMG) {
+            throw new ApiError(500, "Cloudinary upload issue")
+        }
+    
+        const newUser = await User.create({
+            username: username,
+            email: email,
+            fullName: fullName,
+            avatar: avatarIMG.url,
+            coverImage: coverImageIMG?.url || "",
+            password: password,
+        })
+    
+        const createdUser = await User.findById(newUser._id).select("-password -refreshToken -watchHistory").lean()
+    
+        if(!createdUser) throw new ApiError(500, "something went wrong while registering a user")
+    
+    
+        return res.status(201).json(
+            new ApiResponse(200, createdUser, "User Registered successfully")
+        )
 
-    if (!avatarLocalPath) {
-        throw new ApiError(400, "Please upload avatar pic")
+    } catch (error) {
+        if (avatarLocalPath) fs.unlinkSync(avatarLocalPath)
+        if (coverImageLocalPath) fs.unlinkSync(coverImageLocalPath)
+            
+        throw error
     }
-
-    const avatarIMG = await uploadOnCloudidnary(avatarLocalPath)
-    const coverImageIMG = await uploadOnCloudidnary(coverImageLocalPath)
-
-    if (!avatarIMG) {
-        throw new ApiError(400, "Please upload avatar pic")
-    }
-
-    const newUser = await User.create({
-        username: username,
-        email: email,
-        fullName: fullName,
-        avatar: avatarIMG.url,
-        coverImage: coverImageIMG?.url || "",
-        password: password,
-    })
-
-    const createdUser = User.findById(newUser._id).select("-password -refreshToken -watchHistory")
-
-    if(!createdUser) throw new ApiError(500, "something went wrong while registering a user")
-
-
-
-    return res.status(201).json(
-        new ApiResponse(200, createdUser, "User Registered successfully")
-    )
 })
 
 export {registerUser}
