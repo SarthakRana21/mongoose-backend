01ea764a2c4748c34d1cf12f610856c39f040058
= <sarthak.rana0321@gmail.com>
2025-02-27 16:53:14 +0530

    added a global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
@@ -28,10 +28,23 @@ app.get("/", (req, res) => {
 
 // routers
 import userRouter from './routes/user.routes'
+import { ApiError } from './utils/ApiError'
 
 
 // routes decleration
 app.use("/api/v1/users", userRouter)
 // http://localhost:8000/api/v1/users/register
+
+
+// global error handling
+
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+    console.log("Global Error: ", err)
+
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    })
+})
 
 export {app}
