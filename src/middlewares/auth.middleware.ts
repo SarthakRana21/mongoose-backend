33d1ebb0b98c9e2ff53b3b5dad0c0ba20a4f675c
= <sarthak.rana0321@gmail.com>
2025-02-28 15:06:21 +0530

    added auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,33 @@
+import { Request } from "express";
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+    user?: JwtPayload
+}
+
+export const verifyJWT = asyncHandler(async (req: AuthRequest, res, next) => {
+    
+    try {
+        const accesToken = req.cookies?.accessToken || req.headers.authorization?.replace("Bearer ", "");
+        const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as string;
+    
+        if(!accesToken) throw new ApiError(401, "Unauthorized Request")
+    
+        const decodedToken = jwt.verify(accesToken, accessTokenSecret) as JwtPayload
+    
+        const currentUser = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!currentUser) throw new ApiError(401, "Invalid Access Token")
+        
+        req.user = currentUser
+    
+        next();
+        
+    } catch (error) {
+        throw new ApiError(401,"invalid AccessToken")
+    }
+
+})
